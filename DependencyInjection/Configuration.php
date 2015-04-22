57c327488447daf14e6624f427f32da50964d9fc
= <=>
2015-04-22 03:18:30 +0100

    More changes after vendor rename

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('andrew_carter_uk_speedfony');
+        $rootNode = $treeBuilder->root('php_fast_cgi_speedfony');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
---
+++ PHPFastCGISpeedfonyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PHPFastCGI\SpeedfonyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PHPFastCGISpeedfonyBundle extends Bundle
+{
+}
